05d0418afa421be99340bbb90c1376aa7c989a42
Anda Toshiki <hello@toshiki.dev>
2025-10-04 11:13:24 -0700

    !feat(pageinfo): add new pageinfo component below the h1 heading of every doc page for displaying basic information regarding post & add unocss as the optional css resolver

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,17 +3,20 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Artalk from './components/Artalk.vue'
+import PageInfo from './components/PageInfo.vue'
 import Layout from './components/Layout.vue'
 import 'mdit-plugin-callouts/index.css'
 import './styles/style.scss'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
 import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
+import 'uno.css'
 
 export default {
   extends: DefaultTheme,
   Layout,
   enhanceApp({ app }) {
-    app.component('Artalk', Artalk)
+  app.component('Artalk', Artalk)
+  app.component('PageInfo', PageInfo)
 
     app.use(NolebaseGitChangelogPlugin, {
       hideContributorsHeader: true,
---
+++ docs/vite.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig } from 'vite'
+import Components from 'unplugin-vue-components/vite'
+import UnoCSS from 'unocss/vite'
+import { MarkdownTransform } from './.vitepress/plugins/markdown-transform'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'node:path'
+import { createRequire } from 'node:module'
+import type { UserConfig } from 'vite'
+
+const require = createRequire(import.meta.url)
+
+export default defineConfig(async () => {
+    return <UserConfig>{
+        server: {
+            hmr: {
+                overlay: false,
+            },
+            fs: {
+                allow: [resolve(__dirname, '..')],
+            },
+        },
+        plugins: [
+            // custom markdown transformation plugin with regex
+            MarkdownTransform(),
+            // custom components including plugins
+            Components({
+                dirs: ['.vitepress/theme/components'],
+                include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+                resolvers: [
+                    IconsResolver({
+                        componentPrefix: '',
+                    }),
+                    ArcoResolver({ sideEffect: true, resolveIcons: true }),
+                ],
+                dts: './.vitepress/components.d.ts',
+                transformer: 'vue3',
+            }),
+            Icons({
+                compiler: 'vue3',
+                autoInstall: true,
+                defaultStyle: 'display: inline-block',
+            }),
+            UnoCSS(),
+        ],
+        css: {
+            postcss: {
+                plugins: [require('postcss-nested')],
+            },
+        },
+        // ssr: {
+        //     noExternal: ['@andatoshiki/vitepress-plugin-nprogress', '@arco-design/web-vue'],
+        // },
+        build: {
+            chunkSizeWarningLimit: 1600,
+            // rollup build options
+            rollupOptions: {
+                external: ['node:*', './assets/*'],
+            },
+        },
+    }
+})
